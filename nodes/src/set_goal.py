a142ee357065dc44f838d59a5858f9f78170e4a1
MarcelWussow <marcel.wussow@stud.uni-hannover.de>
2024-02-02 09:44:29 +0100

    neue node

--- nodes/src/set_goal.py
+++ nodes/src/set_goal.py
@@ -1,48 +1,59 @@
 #!/usr/bin/env python3
-import rospy # type: ignore
-import actionlib # type: ignore
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal # type: ignore
-#from math import radians, degrees # type: ignore
-from actionlib_msgs.msg import GoalStatus # type: ignore
-from geometry_msgs.msg import Point # type: ignore
+import rospy #type:ignore
+from geometry_msgs.msg import PoseStamped #type:ignore
 
-def move_to_goal(xGoal,yGoal):
+def move_to_goal(xGoal, yGoal):
+    goal_msg = PoseStamped()
+    goal_msg.header.frame_id = "map"
+    goal_msg.header.stamp = rospy.Time.now()
 
-    ac = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+    goal_msg.pose.position.x = xGoal
+    goal_msg.pose.position.y = yGoal
+    goal_msg.pose.position.z = 0.0
 
-    while(not ac.wait_for_server(rospy.Duration.from_sec(5.0))):
-        rospy.loginfo("Waiting for the move_base action server to come up")
+    goal_msg.pose.orientation.x = 0.0
+    goal_msg.pose.orientation.y = 0.0
+    goal_msg.pose.orientation.z = 0.0
+    goal_msg.pose.orientation.w = 1.0
 
-    goal = MoveBaseGoal()
+    return goal_msg
 
-    goal.target_pose.header.frame_id = "map"
-    goal.target_pose.header.stamp = rospy.Time.now()
+#def check_goal_reached():
+    # Simulating the condition when the robot has successfully reached the destination
+    # Replace this with your actual logic or feedback mechanisms
+    #if robot_has_reached_destination_condition:
+        #rospy.loginfo("Robot has successfully reached the destination!")
+        #return True
+    #else:
+        #rospy.loginfo("Robot failed to reach the destination.")
+        #return False
 
-    goal.target_pose.pose.position = Point(xGoal,yGoal,0)
-    goal.target_pose.pose.orientation.x = 0.0
-    goal.target_pose.pose.orientation.y = 0.0
-    goal.target_pose.pose.orientation.z = 0.0
-    goal.target_pose.pose.orientation.w = 1.0
+def main():
+    rospy.init_node("map_navigation_publisher", anonymous=False)
+    goal_publisher = rospy.Publisher("goal_pose", PoseStamped, queue_size=10)
 
-    rospy.loginfo("Sending goal location ...")
-    ac.send_goal(goal)
-
-    ac.wait_for_result(rospy.Duration(60))
-
-    if(ac.get_state() == GoalStatus.SUCCEEDED):
-        rospy.loginfo("You have reached the destnation")
-        return True
-    
-    else:
-        rospy.loginfo("The robot failed to reach the destination")
-        return False
-    
-if __name__ == "__main__":
-    rospy.init_node("map_navigation", anonymous=False)
     x_goal = 7.5
     y_goal = 3
-    print("start go to goal")
-    #move_to_goal(x_goal,y_goal)
-    rospy.spin()
 
+    rate = rospy.Rate(1)  # Publish rate in Hz
 
+    while not rospy.is_shutdown():
+        goal_msg = move_to_goal(x_goal, y_goal)
+        rospy.loginfo("Sending goal location...")
+
+        # Publish goal message
+        goal_publisher.publish(goal_msg)
+        rate.sleep()
+
+        # Log information about goal achievement
+        #rospy.loginfo("Checking if the robot has reached the destination...")
+        #if check_goal_reached():
+            #rospy.loginfo("Robot has successfully reached the destination!")
+        #else:
+            #rospy.loginfo("Robot failed to reach the destination.")
+
+if __name__ == "__main__":
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
